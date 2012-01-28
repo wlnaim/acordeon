b683d734a29a443bf4b4c285623fefb37d0f63e3
Erick Alvarez <e_adrian90@hotmail.com>
2012-01-28 14:53:48 -0600

    empty rails state

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acordeon::Application.config.secret_token = '687821513ef418424b226479795c9bb38ecfbee212577d6ce1d517f6531f26350f037cc628cd7efb412f70a0d7135fd97f0db57ad1f73f3ae6fa51b077f60be9'
